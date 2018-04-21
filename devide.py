551e519122405fdca9a328b618aa911e3c9d3776
daibiaoxuwu <dujl16@mails.tsinghua.edu.cn>
2018-04-21 20:03:03 +0800

    modified:   devide.py 分词到jiebasg,jiebasm里.jiebasg:{'我':500....}单个字符 jiebasm:{'科':{'学':50,'学家':20}....}多个字符 	new file:   pic.py

--- devide.py
+++ devide.py
@@ -1,12 +1,57 @@
+import pickle
+import json
 import jieba
 import jieba.posseg as pseg
 import jieba.analyse
 
-str1 = "我来到北京清华大学"
-str2 = 'python的正则表达式是好用的'
-str3 = "小明硕士毕业于中国科学院计算所，后在日本京都大学深造"
 
-##关键词提取，参数setence对应str1为待提取的文本,topK对应2为返回几个TF/IDF权重最大的关键词，默认值为20
-result4 = jieba.cut_for_search(str3)        ##搜索引擎模式
-for i in result4:
-    print(i)
+#将句子变为"BOSxxxxxEOS"这种形式
+def reform(sentence):
+    #如果是以“。”结束的则将“。”删掉
+    if sentence.endswith("。"):
+        sentence=sentence[:-1]
+    #添加起始符BOS和终止符EOS   
+    sentence_modify1=sentence.replace("。", "EOSBOS")
+    sentence_modify2="BOS"+sentence_modify1+"EOS"
+    return sentence_modify2
+
+def putdict(word,dic):
+    if word in dic:
+        dic[word]+=1
+    else:
+        dic[word]=1
+
+def listtags(text,smdict,sgdict):
+    result = jieba.cut_for_search(text)        ##搜索引擎模式
+    
+    for text in result:
+        if len(text)==1:
+            putdict(text,sgdict)
+        else:
+            smtext=text[1:]
+            if text[0] in smdict:
+                bigdict=smdict[text[0]]
+                if smtext in bigdict:
+                    bigdict[smtext]+=1
+                else:
+                    bigdict[smtext]=1
+            else:
+                smdict[text[0]]={smtext:1}
+    return smdict,sgdict
+
+if __name__ == "__main__":
+
+    #语料句子
+    smdict={}
+    sgdict={}
+    for _ in range(1,12):
+        #text=open('../sina_news/2016-%02d.txt'%_).read()
+        for text in open('../sina_news/2016-%02d.txt'%_).readlines():
+            smdict,sgdict=listtags(json.loads(text)['html'],smdict,sgdict)
+            smdict,sgdict=listtags(json.loads(text)['title'],smdict,sgdict)
+        print('one')
+    with open('../jiebasm','wb') as f:
+        pickle.dump(smdict,f)
+    with open('../jiebasg','wb') as f:
+        pickle.dump(sgdict,f)
+
---
+++ pic.py
@@ -0,0 +1,3 @@
+import pickle
+with open('jiebasm','rb') as f:
+    print(pickle.load(f))
